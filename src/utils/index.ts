f15d1cd1d5abd11a9ea444762e56de007547c06b
wangmaomao <hisoftworm@hotmail.com>
2025-04-14 16:27:31 +0800

    refactor: key变量替换时，多个参数加空格

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -582,7 +582,7 @@ export class Utils {
         const keyStr = isSingleQuote ? `'${key}'` : `"${key}"`;
         let i18nT = `${suffix}(${keyStr})`;
         if (varObj[char]) {
-          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList}])`;
+          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList.toString(", ")}])`;
         }
         return i18nT;
       };
@@ -711,7 +711,7 @@ export class Utils {
         const keyStr = isSingleQuote ? `'${key}'` : `"${key}"`;
         let i18nT = `${suffix}(${keyStr})`;
         if (varObj[char]) {
-          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList}])`;
+          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList.toString(", ")}])`;
         }
         return i18nT;
       };
@@ -767,7 +767,7 @@ export class Utils {
         const keyStr = isSingleQuote ? `'${key}'` : `"${key}"`;
         let i18nT = `${suffix}(${keyStr})`;
         if (varObj[char]) {
-          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList}])`;
+          i18nT = `${suffix}(${keyStr}, [${varObj[char].varList.toString(", ")}])`;
         }
         return i18nT;
       };
